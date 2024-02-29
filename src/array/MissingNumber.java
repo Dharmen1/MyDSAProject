4beecc736bbbb0079accdb956134ee07a59ae00f
Dharmendra <dharmenk412@gmail.com>
2024-02-29 23:41:16 +0530

    Valid Parenthesis code

--- src/array/MissingNumber.java
+++ src/array/MissingNumber.java
@@ -8,18 +8,18 @@ public class MissingNumber {
         int b = 0;
 
         /*For xor of all the elements in array */
-        for (int i = 1; i <n; i++)
+        for (int i = 0; i <n; i++)
             a = a ^ arr[i];
 
         /* For xor of all the elements from 1 to n+1 */
-        for (int i = 1; i < n + 1; i++)
+        for (int i = 0; i < n + 1; i++)
             b = b ^ i;
 
         return (a ^ b);
     }
 
     public static void main(String[] args) {
-        int[] a = {1, 2, 4, 5, 6};
+        int[] a = {3,0,1};
         System.out.println(findMissingNumber(a));
     }
 }
